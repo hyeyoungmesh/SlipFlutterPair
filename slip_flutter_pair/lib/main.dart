b2c80965f938213ee09fd1d741c3d83f4d5aa7c6
hyeyoung.moon <hyeyoung.moon@meshkorea.net>
2023-10-12 21:12:42 +0900

    hy job

--- slip_flutter_pair/lib/main.dart
+++ slip_flutter_pair/lib/main.dart
@@ -31,28 +31,80 @@ class MyTimelineListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: const [
-        MyTimelineTile(isFirst: true, isLast: false, content: "content")
+        MyTimelineTile(
+          isFirst: true,
+          isLast: false,
+          isPast: true,
+          content: "content",
+        ),
+        MyTimelineTile(
+          isFirst: false,
+          isLast: false,
+          isPast: true,
+          content: "content",
+        ),
+        MyTimelineTile(
+          isFirst: false,
+          isLast: true,
+          isPast: false,
+          content: "content",
+        ),
       ],
     );
   }
 }
 
 class MyTimelineTile extends StatelessWidget {
-  const MyTimelineTile(
-      {super.key,
-      required this.isFirst,
-      required this.isLast,
-      required this.content});
+  const MyTimelineTile({
+    super.key,
+    required this.isFirst,
+    required this.isLast,
+    required this.isPast,
+    required this.content,
+  });
   final bool isFirst;
   final bool isLast;
+  final bool isPast;
   final String content;
 
   @override
   Widget build(BuildContext context) {
-    return TimelineTile(
-      isFirst: isFirst,
-      isLast: isLast,
-      endChild: Text(content),
+    return SizedBox(
+      height: 200,
+      child: Container(
+        child: TimelineTile(
+          isFirst: isFirst,
+          isLast: isLast,
+          endChild: DecorationBoxWidget(),
+          beforeLineStyle: LineStyle(
+            color: isPast ? Colors.purple : Colors.purple.shade100,
+          ),
+          indicatorStyle: IndicatorStyle(
+            width: 25,
+            height: 25,
+            color: isPast ? Colors.purple : Colors.purple.shade100,
+            iconStyle: IconStyle(
+              color: isPast ? Colors.white : Colors.purple.shade100,
+              iconData: Icons.done,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DecorationBoxWidget extends StatelessWidget {
+  const DecorationBoxWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SizedBox(
+        width: 100,
+        height: 100,
+        child: Text('text'),
+      ),
     );
   }
 }
